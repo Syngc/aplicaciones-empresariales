115067339bc4c4e55706d5789b20563adc305156
Allan <rpamplona.allan@gmail.com>
2018-08-01 15:18:49 -0500

    Prod server

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+app.use( express.static( `${__dirname}/build` ) );
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/build/index.html'));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
