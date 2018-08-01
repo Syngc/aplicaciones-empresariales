6ae2376ec5a10dc3b39dbb591ff4536254250a1f
Allan <rpamplona.allan@gmail.com>
2018-08-01 16:08:29 -0500

    Heroku

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,23 +1,22 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import {apis} from '../config'
-
+console.log(process.env.REACT_APP_INTEGRADOR_MESSAGING)
 const prodConfig = {
-  apiKey: apis.apiKey,
-  authDomain: apis.authDomain,
-  databaseURL: apis.databaseURL,
-  projectId: apis.projectId,
-  storageBucket: apis.storageBucket,
-  messagingSenderId: apis.messagingSenderId
+  apiKey: process.env.REACT_INTEGRADOR_API_KEY,
+  authDomain: process.env.REACT_INTEGRADOR_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_INTEGRADOR_DATABASE,
+  projectId: process.env.REACT_INTEGRADOR_PROJECT_ID,
+  storageBucket: process.env.REACT_INTEGRADOR_STORAGE,
+  messagingSenderId: process.env.REACT_INTEGRADOR_MESSAGING
 };
 
 const devConfig = {
-  apiKey: apis.apiKey,
-  authDomain: apis.authDomain,
-  databaseURL: apis.databaseURL,
-  projectId: apis.projectId,
-  storageBucket: apis.storageBucket,
-  messagingSenderId: apis.messagingSenderId
+  apiKey: process.env.REACT_APP_INTEGRADOR_API_KEY,
+  authDomain: process.env.REACT_APP_INTEGRADOR_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_INTEGRADOR_DATABASE,
+  projectId: process.env.REACT_APP_INTEGRADOR_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_INTEGRADOR_STORAGE,
+  messagingSenderId: process.env.REACT_APP_INTEGRADOR_MESSAGING
 };
 
 const config = process.env.NODE_ENV === 'production'
