39965197d265d5d1bb80c950bb7010e7468b4848
Allan <rpamplona.allan@gmail.com>
2018-08-01 16:21:35 -0500

    Fix

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/build/index.html'));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT, function () {
+  console.log('Example app listening on port ' + process.env.PORT);
 });
-
